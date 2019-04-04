d1333c35039be2dbc842067133381d3d984f6869
NERD-X <nerdxontheline@outlook.com>
2019-04-04 20:19:41 +0100

    removeObjects Method

--- FP8/2019.pp.fp08/src/ObjectManagement/ContainerOfObjects.java
+++ FP8/2019.pp.fp08/src/ObjectManagement/ContainerOfObjects.java
@@ -6,34 +6,80 @@
 package ObjectManagement;
 
 /**
- * Qualquer Objeto é uma extensão de Object por isso esta classe pode receber qualquer tipo de objecto 
+ * Qualquer Objeto é uma extensão de Object por isso esta classe pode receber
+ * qualquer tipo de objecto
+ *
  * @author NERD-X
  */
 public class ContainerOfObjects {
+
     private final int DEFAULT_SIZE = 100;
     private Object[] object;
+    private int counter = 0;
 
     /**
      * Metodo Constructor que recebe um array de objeto para definir object
+     *
      * @param object
      */
     public ContainerOfObjects(Object[] object) {
         this.object = object;
     }
-    
+
     /**
-     * Metodo Constructor Default que define o tamanho máximo através da constante
+     * Metodo Constructor Default que define o tamanho máximo através da
+     * constante
      */
     public ContainerOfObjects() {
         this.object = new Object[DEFAULT_SIZE];
     }
-    
+
     /**
-     * Metodo Constructor que recebe um tamanho máximo para atribuir ao array de objects
+     * Metodo Constructor que recebe um tamanho máximo para atribuir ao array de
+     * objects
+     *
      * @param maxSize
      */
     public ContainerOfObjects(int maxSize) {
         this.object = new Object[maxSize];
     }
-    
+
+    protected boolean addObjects(Object newobject) {
+        if (counter < object.length) {
+            object[counter] = newobject;
+            counter += 1;
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    protected Object removeObjects(int position) {
+
+        if (position < object.length) {
+            if (object[position] == null) {
+                System.out.println("Objecto não existe");
+                return null;
+            } else {
+                Object[] tmp = new Object[object.length];
+                Object delete = object[position];
+                int tmpPosition = 0;
+                for (int i = 0; i <= counter; i++) {
+                    if (i != position) {
+                        tmp[tmpPosition] = object[i];
+                        tmpPosition += 1;
+                    }
+                }
+                object = tmp;
+                counter -= 1;
+                return delete;
+            }
+        } else {
+            System.out.println("Posição Inexistente");
+            return null;
+        }
+        
+    }
+
 }
---
+++ FP8/2019.pp.fp08/src/ObjectManagement/Demo.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ObjectManagement;
+
+/**
+ *
+ * @author NERD-X
+ */
+public class Demo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Object obj1 = new Object();
+        Object obj2 = new Object();
+        Object obj3 = new Object();
+        Object obj4 = new Object();
+        Object obj5 = new Object();
+        
+        ContainerOfObjects teste = new ContainerOfObjects();
+        teste.addObjects(obj1);
+        teste.addObjects(obj2);
+        teste.addObjects(obj3);
+        teste.addObjects(obj4);
+        teste.addObjects(obj5);
+        
+        teste.removeObjects(1);
+        System.out.println("Hello World!");
+    }
+    
+}
