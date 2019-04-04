c4456c541422266f6e309cd7f5ead347f0f3a9ed
NERD-X <nerdxontheline@outlook.com>
2019-04-04 18:02:10 +0100

    Change Directory

--- FP8/2019.pp.fp08/src/ObjectManagement/ContainerOfObjects.java
+++ FP8/2019.pp.fp08/src/ObjectManagement/ContainerOfObjects.java
@@ -6,27 +6,34 @@
 package ObjectManagement;
 
 /**
- *
+ * Qualquer Objeto é uma extensão de Object por isso esta classe pode receber qualquer tipo de objecto 
  * @author NERD-X
  */
 public class ContainerOfObjects {
     private final int DEFAULT_SIZE = 100;
     private Object[] object;
 
+    /**
+     * Metodo Constructor que recebe um array de objeto para definir object
+     * @param object
+     */
     public ContainerOfObjects(Object[] object) {
         this.object = object;
     }
     
+    /**
+     * Metodo Constructor Default que define o tamanho máximo através da constante
+     */
     public ContainerOfObjects() {
         this.object = new Object[DEFAULT_SIZE];
     }
     
+    /**
+     * Metodo Constructor que recebe um tamanho máximo para atribuir ao array de objects
+     * @param maxSize
+     */
     public ContainerOfObjects(int maxSize) {
         this.object = new Object[maxSize];
     }
-    
-    
-    
-    
     
 }
